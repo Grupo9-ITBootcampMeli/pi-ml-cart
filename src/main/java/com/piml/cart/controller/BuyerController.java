a01378f608ce3237b7e52f7fbb6564e1f4825a0e
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-05-05 15:00:20 -0300

    doc: added oas and readmefile

--- src/main/java/com/piml/cart/controller/BuyerController.java
+++ src/main/java/com/piml/cart/controller/BuyerController.java
@@ -3,11 +3,14 @@ package com.piml.cart.controller;
 import com.piml.cart.dto.BuyerDto;
 import com.piml.cart.dto.BuyerResponseDto;
 import com.piml.cart.service.BuyerApiService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@Api(value = "Buyer")
 @RestController
 @RequestMapping
 public class BuyerController {
@@ -17,6 +20,7 @@ public class BuyerController {
         this.buyerApiService = buyerApiService;
     }
 
+    @ApiOperation(value = "Register a new Buyer")
     @PostMapping("/buyer/v1")
     public ResponseEntity<BuyerResponseDto> createBuyer(@RequestBody BuyerDto buyer) {
         BuyerDto createdBuyer = buyerApiService.create(buyer);
@@ -24,6 +28,7 @@ public class BuyerController {
         return new ResponseEntity<>(returnBuyer, HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Find Buyer by ID")
     @GetMapping("/buyer/v1/{id}")
     public ResponseEntity<BuyerDto> getBuyerById(@PathVariable Long id) {
         BuyerDto foundBuyer = buyerApiService.getById(id);
